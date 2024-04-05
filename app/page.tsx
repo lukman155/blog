e78413dbd544eae89e26fe79075e90fc45a80379
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2024-04-05 23:51:13 +0100

    Fix - Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import ArticleItemList from "@/components/ArticleListItem";
+import ArticleModal from "./Components/ArticleModel";
 import { getCategorisedArticles } from "./dummyData";
 
 const Home = () => {
@@ -15,7 +15,7 @@ const Home = () => {
       <section className="article-section">
         {articles !== null &&
           Object.keys(articles).map((article) => (
-            <ArticleItemList
+            <ArticleModal
               category={article}
               articles={articles[article]}
               key={article}
@@ -26,9 +26,8 @@ const Home = () => {
       {/* Styled-JSX Styles */}
       <style jsx>{`
         .container {
-          margin-left: auto;
-          margin-right: auto;
-          width: 91.666667%;
+          margin: 0 auto;
+          max-width: 800px;
           margin-top: 5rem;
           display: flex;
           flex-direction: column;
@@ -36,18 +35,29 @@ const Home = () => {
           margin-bottom: 5rem;
         }
         .title {
-          font-family: 'Cormorant Garamond', serif;
           font-weight: 300;
-          font-size: 4rem;
+          font-size: 2rem;
           color: #333333;
           text-align: center;
         }
         .article-section {
+          padding: 1rem;
           display: grid;
-          grid-template-columns: repeat(2, minmax(0, 1fr));
+          grid-template-columns: repeat(1, minmax(0, 1fr));
         }
-        .article-section > :not([hidden]) ~ :not([hidden]) {
-          margin-top: 0;
+        @media (min-width: 580px) {
+          .title {
+          font-size: 4rem;
+        }
+          .article-section {
+            grid-template-columns: repeat(2, minmax(0, 1fr));
+          }
+        }
+
+        @media (min-width: 1080px) {
+          .article-section {
+            grid-template-columns: repeat(3, minmax(0, 1fr));
+          }
         }
       `}</style>
     </section>
